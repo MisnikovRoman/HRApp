878217c03b49370bf07a02ebfaae94e49b32fa27
Roman <misnikovroman@gmail.com>
2018-08-13 17:25:05 +0300

    Successfully loaded json data from server and showed on screen

--- HRApp/Vacancy/Controller/VacanciesVC.swift
+++ HRApp/Vacancy/Controller/VacanciesVC.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class VacanciesVC: UIViewController {
 
+    private var vacancies: [Vacancy] = []
+
+    @IBOutlet weak var vacanciesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,6 +23,8 @@ class VacanciesVC: UIViewController {
             case .success(let data):
                 guard let vacancies = data as? [Vacancy] else { return }
                 vacancies.forEach { $0.printDescription() }
+                self.vacancies = vacancies
+                self.vacanciesTableView.reloadData()
             case .failure(let error):
                 print(error)
             }
@@ -34,12 +40,12 @@ extension VacanciesVC: UITableViewDelegate {
 
 extension VacanciesVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return vacancies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: VACANCY_CELL, for: indexPath)
-        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: VACANCY_CELL, for: indexPath) as? VacancyCell else { return UITableViewCell() }
+        cell.setup(withModel: vacancies[indexPath.row])
         return cell
     }
     
